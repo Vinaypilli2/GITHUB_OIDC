724face7791be6279d9cfc3179d5edefb4bcdca3
Vinaypilli2 <vinaypilli56@gmail.com>
2025-05-05 15:20:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,28 @@
-provider "google" {
-  project = "direct-outlet-458407-n6"
-  region  = "us-central1"
-}
-
 resource "google_iam_workload_identity_pool" "github_pool" {
+  project        = "direct-outlet-458407-n6"
+  location       = "global"
   workload_identity_pool_id = "github-pool"
-  display_name              = "GitHub Pool"
-  description               = "OIDC pool for GitHub Actions"
-  location                  = "global"
+  display_name   = "GitHub Pool"
+  description    = "OIDC pool for GitHub Actions"
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  project                            = "direct-outlet-458407-n6"
+  location                           = "global"
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-provider"
-  display_name                       = "GitHub Provider"
-  description                        = "Allows GitHub Actions OIDC tokens"
-  location                           = "global"
+
+  display_name = "GitHub OIDC Provider"
+  description  = "OIDC provider for GitHub Actions"
+
+  attribute_mapping = {
+    "google.subject"         = "assertion.sub"
+    "attribute.actor"        = "assertion.actor"
+    "attribute.repository"   = "assertion.repository"
+  }
 
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-
-  attribute_mapping = {
-    "google.subject"           = "assertion.sub"
-    "attribute.repository"     = "assertion.repository"
-    "attribute.actor"          = "assertion.actor"
-    "attribute.aud"            = "assertion.aud"
   }
 
   attribute_condition = "assertion.repository == 'Vinaypilli2/GITHUB_OIDC'"
